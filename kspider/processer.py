b66480af5940e77d9d2580f9ed90602fde1c3b06
kzlKnight <510989510@qq.com>
2020-05-28 14:39:25 +0800

    new

--- kspider/processer.py
+++ kspider/processer.py
@@ -1,11 +1,21 @@
 from kspider.settings import ScheduleSettings
 from kspider.settings import ProcesserSettings
 from kspider.shedule import Schedule
+from kspider.commen import stop_thread
 from kspider.pool import ProxyPool
 from kspider.http import Request,Response
 from threading import Thread
 from queue import Queue
+import time
 
+
+# todo
+'''
+1. 发送request失败，调用errorback
+2. 解析response失败，调用某个中间件
+3. download_middleware设置default header
+4. yield request|data data可以交给管道文件
+'''
 
 class Processer():
     def __init__(
@@ -29,17 +39,25 @@ class Processer():
         self.excute_request_thread_targets = []
         self.excute_response_thread_targets = []
 
+
+        # 解析request与response线程对象的状态
+        self.excute_request_thread_statements = []
+        self.excute_response_thread_statements = []
+
+
         # 生成解析request的线程对象
         for i in range(processerSettings.EXCUTE_THREAD['excute_request_thread_num']):
             self.excute_request_thread_targets.append(
-                Thread(target=self.excute_request)
+                Thread(target=self.excute_request,args=(i,))
             )
+            self.excute_request_thread_statements.append(0)
 
         # 生成解析response的线程对象
         for i in range(processerSettings.EXCUTE_THREAD['excute_response_thread_num']):
             self.excute_response_thread_targets.append(
-                Thread(target=self.excute_response)
+                Thread(target=self.excute_response,args=(i,))
             )
+            self.excute_response_thread_statements.append(0)
 
         # 代理IP池的管理
         # 如果使用了代理IP池
@@ -55,10 +73,14 @@ class Processer():
         self.excute_response_queue = Queue()
 
     # 处理请求
-    def excute_request(self):
+    def excute_request(self,statement_index):
         while True:
+            # 未获得request时，线程状态为0
+            self.excute_request_thread_statements[statement_index] = 0
             # 得到request对象
             request = self.schedule.get() # 堵车状态
+            # 获得request时，线程状态为1
+            self.excute_request_thread_statements[statement_index] = 1
             try:
                 request_response = request.get_response(
                     max_retry_num=self.__request_max_retry_num,
@@ -73,26 +95,85 @@ class Processer():
                 # todo 拆分异常
                 self.schedule.put_error(request=request)
 
-
-    def excute_response(self):
+    # 处理异常
+    def excute_response(self,statement_index):
         while True:
+            # 未获得response，状态为0
+            self.excute_response_thread_statements[statement_index] = 0
+            # 得到response
             response = self.excute_response_queue.get()
+            # 获得response，状态为1
+            self.excute_response_thread_statements[statement_index] = 1
+            # 尝试执行回调函数
             try:
                 callback_func = eval('self.spider.{callback}'.format(callback=response.request.callback))
                 for request in callback_func(response):
                     self.schedule.put(request)
             except Exception as e:
                 # ******************
+                # todo spider error
                 print(e)
                 # ******************
 
-    def run(self):
-        for request in self.spider.start_request():
-            self.schedule.put(request)
+    def __run(self):
+        try:
+            for request in self.spider.start_request():
+                self.schedule.put(request)
+        except Exception as e:
+            # **********
+            # todo spider error
+            print(e)
+            # *********
 
         for excute_request_thread_target in self.excute_request_thread_targets:
             excute_request_thread_target.start()
 
+
         for excute_response_thread_target in self.excute_response_thread_targets:
             excute_response_thread_target.start()
 
+        # 检查是否执行完毕
+        def check_excute():
+            def check_excute_wrapper():
+                for request_statement in self.excute_request_thread_statements:
+                    if request_statement == 1:
+                        return False
+                for request_statement in self.excute_response_thread_statements:
+                    if request_statement == 1:
+                        return False
+                return True
+
+            for i in range(3):
+                if not check_excute_wrapper():return False
+                time.sleep(3)
+            return True
+
+        # 验证是否执行完毕，如果执行完毕，跳出循环
+        while True:
+            if check_excute():
+                break
+
+        # 关闭
+        self.close()
+
+
+    # 开始
+    def run(self):
+        self.excute_run_thread_target = Thread(target=self.__run)
+        self.excute_run_thread_target.start()
+
+
+    def close(self):
+        for thread_target in self.excute_request_thread_targets:
+            stop_thread(thread_target)
+        for thread_target in self.excute_response_thread_targets:
+            stop_thread(thread_target)
+        try:
+            stop_thread(self.excute_run_thread_target)
+        except:pass
+
+
+
+                
+            
+            
